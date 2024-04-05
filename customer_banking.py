434305f89d192cae197f61876f78dfca63df3879
Joe Middleton <162524521+JoeMiddleton14@users.noreply.github.com>
2024-04-04 21:49:12 -0600

    Update customer_banking.py
    updated code

--- customer_banking.py
+++ customer_banking.py
@@ -23,23 +23,7 @@ def main():
     print(f"Interest earned: ${cd_interest_earned}")
     print(f"Updated balance after {cd_months} months: ${cd_updated_balance}")
 
-    """This function prompts the user to enter the savings and cd account balance, interest rate,
-    and the length of months to determine the interest gained.
-    It displays the interest earned on the savings and CD accounts and updates the balances.
-    """
 
-    cd_balance = float(input("What is your initial CD balance? "))
-    interest_rate = float(input("What is your APR? "))
-    cd_months = int(input("What is the length of months for savings account? "))
-
-    # Prompt the user to set the savings balance, interest rate, and months for the savings account.
-    savings_balance = float(input("What is your initial savings account balance? "))
-    savings_interest_rate = float(input("What is your APR for your savings account? "))
-    savings_months = int(input("What is the length of months for savings account? "))
-
-
-    # Call the create_savings_account function and pass the variables from the user.
-    create_savings_account = create_savings_account(savings_balance, savings_interest_rate, savings_months)
 
 if __name__ == "__main__":
     # Call the main function.
